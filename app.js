bfdabb8513b564642ce6f03032e2f02e83ea5e6c
LucianaRivarossa <lucianarivarossa@gmail.com>
2022-08-25 16:55:20 -0300

    se agrega cambio en texto

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ let promedio =0
 let suma=0
 for (let i=1; i<= cantidadAlumnos;i++){
     let nota = Number(prompt(`Ingrese nota del alumno ${i}`))
+    let nombre = Number(prompt(`Ingrese nombre del alumno ${i}`))
     if (nota<6)
     {
         d++
-        alert(`alumno desaprobado`)
+        alert(`El alumno ${nombre} está desaprobado`)
     }   
     else 
     {
         a++
-        alert(`alumno aprobado`)
+        alert(`El alumno ${nombre} está aprobado`)
     }
     suma +=nota
     promedio = suma/cantidadAlumnos
 }
 alert(`La cantidad de alumnos desaprobados es ${d}`)
 alert(`La cantidad de alumnso aprobados es ${a}`)
-alert(`El promedio de notas de los alumnos es ${promedio}`)
+alert(`El promedio de notas del curso completo es  ${promedio}`)
