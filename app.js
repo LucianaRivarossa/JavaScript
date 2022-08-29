6b734e36e1eda64eb3a1e1150bac3be0d42bbeff
LucianaRivarossa <lucianarivarossa@gmail.com>
2022-08-29 19:22:35 -0300

    agrego tienda de ropa

--- app.js
+++ app.js
@@ -1,24 +1,119 @@
-let cantidadAlumnos = Number(prompt("Ingrese la cantidad de alumnos"))
-let d=0
-let a=0
-let promedio =0
-let suma=0
-for (let i=1; i<= cantidadAlumnos;i++){
-    let nota = Number(prompt(`Ingrese nota del alumno ${i}`))
-    let nombre = Number(prompt(`Ingrese nombre del alumno ${i}`))
-    if (nota<6)
-    {
-        d++
-        alert(`El alumno ${nombre} está desaprobado`)
-    }   
-    else 
-    {
-        a++
-        alert(`El alumno ${nombre} está aprobado`)
-    }
-    suma +=nota
-    promedio = suma/cantidadAlumnos
+function iniciar(){
+    alert(`Tienda de ropa`)
 }
-alert(`La cantidad de alumnos desaprobados es ${d}`)
-alert(`La cantidad de alumnso aprobados es ${a}`)
-alert(`El promedio de notas del curso completo es  ${promedio}`)
+function saludar(nombre){
+    alert(`Bienvenido ${nombre}`)
+}
+function mostrarMenu(){
+    let categoria = Number(prompt(`¿Qué estás buscando? \n 1. Vestidos \n 2. Remeras \n 3. Jeans \n 4. Sweaters \n 5. Salir `))
+    return categoria
+}
+function mostrarVestidos(){
+    let opcion = Number(prompt(`VESTIDOS \n 1. Vestido Rojo \n 2. Vestido Verde \n 3. Vestido Azul \n 4. Vestido Amarillo \n 5. Volver al menú anterior`))
+    return opcion
+}
+function mostrarRemeras(){
+    let opcion = Number(prompt(`REMERAS \n 1. Remera Roja \n 2. Remera Verde \n 3. Remera Azul \n 4. Remera Amarilla \n 5. Volver al menú anterior`))
+    return opcion
+}
+function mostrarJeans(){
+    let opcion = Number(prompt(`JEANS \n 1. Jean Rojo \n 2. Jean Verde \n 3. Jean Azul \n 4. Jean Amarillo \n 5. Volver al menú anterior`))
+    return opcion
+}
+function mostrarSweaters(){
+    let opcion = Number(prompt(`SWEATERS \n 1. Sweater Rojo \n 2. Sweater Verde \n 3. Sweater Azul \n 4. Sweater Amarillo \n 5. Volver al menú anterior`))
+    return opcion
+}
+function comprarPrenda(prenda,precio){
+    alert (`Compraste ${prenda} a $${precio}`)
+    let seguirComprando=false
+    respuesta = prompt("¿Desea seguir comprando?(si/no)")
+    if (respuesta === "si" || respuesta === "Si" || respuesta === "SI")
+        seguirComprando =true
+    return seguirComprando
+}
+function salir(){
+    alert(`Gracias por su visita`)
+}
+iniciar()
+nombre= prompt(`Ingrese nombre de usuario`)
+contraseña= prompt(`Ingrese contraseña`)
+let opcion = 0
+let categoria=0
+let seguirComprando=true
+if (contraseña === "123"){
+    saludar(nombre)
+    while(seguirComprando){
+        categoria =mostrarMenu()
+        console.log(`${categoria}`)
+        switch(categoria){
+            case 1: 
+                opcion = Number(mostrarVestidos())
+            
+                if (opcion != 5)
+                    if (opcion === 1)
+                        seguirComprando= comprarPrenda("Vestido Rojo",1000)
+                    else if (opcion === 2)
+                        seguirComprando=comprarPrenda("Vestido Verde",5000)
+                    else if (opcion === 3)
+                        seguirComprando=  comprarPrenda("Vestido Azul",4000)
+                    else if (opcion === 4)
+                        seguirComprando=comprarPrenda("Vestido Amarillo",1000)
+                else 
+                    {   console.log("entre")
+                        mostrarMenu()}
+                break
+            case 2: 
+                opcion = mostrarRemeras()
+                if (opcion != 5)
+                    if (opcion === 1)
+                          seguirComprando=comprarPrenda("Remera Roja",1000)
+                    else if (opcion === 2)
+                         seguirComprando=comprarPrenda("Remera Verde",2000)
+                    else if (opcion === 3)
+                         seguirComprando=comprarPrenda("Remera Azul",4000)
+                    else if (opcion === 4)
+                         seguirComprando=comprarPrenda("Remera Amarilla",5000)
+                else 
+                    {mostrarMenu()}
+                break
+            case 3:
+                opcion = mostrarJeans()
+                if (opcion != 5)
+                    if (opcion === 1)
+                          seguirComprando= comprarPrenda("Jean Rojo",4000)
+                    else if (opcion === 2)
+                         seguirComprando=  comprarPrenda("Jean Verde",3000)
+                    else if (opcion === 3)
+                          seguirComprando=  comprarPrenda("Jean Azul",2000)
+                    else if (opcion === 4)
+                         seguirComprando= comprarPrenda("Jean Amarillo",4000)
+                else 
+                    {mostrarMenu()}
+                break
+            case 4: 
+                opcion = mostrarSweaters()
+                if (opcion != 5)
+                    if (opcion === 1)
+                         seguirComprando=comprarPrenda("Sweater Rojo",1000)
+                    else if (opcion === 2)
+                         seguirComprando= comprarPrenda("Sweater Verde",2000)
+                    else if (opcion === 3)
+                         seguirComprando= comprarPrenda("Sweater Azul",3000)
+                    else if (opcion === 4)
+                         seguirComprando= comprarPrenda("Sweater Amarillo",1000)
+                else 
+                    {mostrarMenu()}
+                
+                break
+            case 5: 
+                seguirComprando =false
+                break
+            default: 
+                alert(`Opción incorrecta. Vuelva a seleccionar la opción`)
+                categoria= mostrarMenu()
+        }
+    }
+    salir()
+}
+
