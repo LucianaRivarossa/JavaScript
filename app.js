aafab5803b738f515ffa8363b7a1b9bbecee196b
LucianaRivarossa <lucianarivarossa@gmail.com>
2022-09-08 21:54:21 -0300

    agrego funciones de orden superior

--- app.js
+++ app.js
@@ -5,48 +5,55 @@ function saludar(nombre){
     alert(`Bienvenido ${nombre}`)
 }
 function mostrarMenu(){
-    let categoria = Number(prompt(`¿Qué desea comprar? \n 1. Vestido $5.000 \n 2. Remera $2.000\n 3. Jean $6.000\n 4. Sweater $1.000 \n 5. Salir `))
-    return categoria
+    opcion = prompt(`¿Qué desea comprar? \n 1. Vestido $5.000 \n 2. Remera $2.000\n 3. Jean $6.000\n 4. Sweater $1.000 \n 5. Producto Exclusivo en oferta \n 6. Salir `)
 }
 function calcularTotal(productos){
-    let totalcompra = 0
-    for (let i = 0 ; i< productos.length ; i++){
-        totalcompra = totalcompra + productos[i].precio * productos[i].cantidad
-    }
+    let totalcompra = productos.reduce((accum, producto) => {
+       return accum + producto.precio * producto.cantidad 
+    },0);
     return totalcompra
 }
 function listarCompra(productos){
     let lista = ""
-    for (let i = 0 ; i< productos.length ; i++){
-        lista = lista + productos[i].cantidad + " " + productos[i].nombre +"\n "
-    }
+    //for (let i = 0 ; i< productos.length ; i++){
+      //  lista = lista + productos[i].cantidad + " " + productos[i].nombre +"\n "
+    //}
+    //return lista
+    productos.forEach((producto) => {
+        lista += producto.cantidad + " " + producto.nombre +"\n "
+    });
     return lista
 }
-function comprarProducto(codigo_producto){
+function comprarProducto(){
     let cantidad=0
     nombre=""
     precio=0
-    if (codigo_producto ===1){
+    if (opcion ==="1"){
         nombre= "Vestido"
         precio = 5000
-        console.log("entro")
     }
-    else if (codigo_producto ===2){
+    else if (opcion ==="2"){
         nombre= "Remera"
         precio = 2000
     }
-    else if (codigo_producto ===3){
+    else if (opcion ==="3"){
         nombre= "Jean"
         precio=6000
     }
-    else if (codigo_producto ===4){
+    else if (opcion ==="4"){
         nombre= "Sweater"
         precio=1000
     }
+    else if (opcion=== "5"){
+        const filtrado = productosExclusivos.filter((producto) => producto.precio < 12000);
+        for (const filtra of filtrado){
+            nombre = filtra.nombre
+            precio = filtra.precio
+        }       
+    }
     cantidad= prompt(`Ingrese cantidad de ${nombre} a comprar a $${precio} cada uno `)
-        let articuloIngresado = new Producto(nombre, precio, cantidad);
-        productos.push(articuloIngresado);
-        
+    let articuloIngresado = new Producto(nombre, precio, cantidad);
+    productos.push(articuloIngresado);     
 }
 function salir(){
     alert(`Gracias por su visita`)
@@ -55,7 +62,6 @@ iniciar()
 nombre= prompt(`Ingrese nombre de usuario`)
 contraseña= prompt(`Ingrese contraseña`)
 let opcion = 0
-let codigo_producto=0
 const productos = [];
 class Producto {
     constructor(nombre, precio, cantidad) {
@@ -64,27 +70,33 @@ class Producto {
       this.cantidad = cantidad;
     }
   }
+const productosExclusivos =[
+    {nombre:"Vestido Exclusivo", precio: 15000 , cantidad: 1000},
+    {nombre:"Remera Exclusivo", precio: 12000 , cantidad: 1000},
+    {nombre:"Jean Exclusivo", precio: 16000 , cantidad: 1000},
+    {nombre:"Sweater Exclusivo", precio: 11000 , cantidad: 1000},
+  ];
 if (contraseña === "123"){
     saludar(nombre)
-    while(codigo_producto != 5){
-        codigo_producto=mostrarMenu()
-        console.log(codigo_producto)
-        comprarProducto(codigo_producto)
+    mostrarMenu()
+    while(opcion != 6){
+        comprarProducto()
         respuesta = prompt("¿Desea seguir comprando?(si/no)")
         if (respuesta === "si" || respuesta === "Si" || respuesta === "SI"){
-            codigo_producto = 0
+            mostrarMenu()
         }
         else if (respuesta === "no" || respuesta === "No" || respuesta === "NO")
        {
-            codigo_producto =5
-       }
-       
+            opcion ="6"
+       }   
     }
     total_compra = calcularTotal(productos)
     lista = listarCompra(productos)
-    alert(`Usted compró:\n ${lista}`)
-    alert(`El total de la compra es: $${total_compra}`)
+    if (lista != "" && total_compra != 0)
+    {
+        alert(`Usted compró:\n ${lista}`)
+        alert(`El total de la compra es: $${total_compra}`)
+    }
     salir()
-    
 }
 
