51250fb40e01a78a4cbb6076e33896cc3e89c8f9
LucianaRivarossa <lucianarivarossa@gmail.com>
2022-11-14 23:37:30 -0300

    agrego modificaciones estéticas

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
+let CantidadDeProductos = 0;
+
+const menu = document.getElementById("encabezado")
+  const nav = document.createElement("nav")
+
+  const actualizarCantidadDeProductos = (cantidad) =>{
+
+ 
+  menu.innerHTML=""
+  nav.classList.add("navbar","navbar-expand-lg","bg-light")
+  console.log()
+  nav.innerHTML=`<div class="container-fluid ">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarText">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Winter Sale</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">New Collection</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">All Shop</a>
+      </li>
+      <a class="navbar-brand titulo "><span><strong>Mora Indumentaria</strong></span> </a>
+    </ul>
+    <button onclick=dibujarcarrito() type="button" class="btn btn-secondary carrito_notificacion" >
+        <i class="fa-solid fa-cart-shopping"></i>
+        <span id="carrito_cantidad" class="badge text-bg-secondary">${cantidad}</span>
+    </button>
+    <span class="navbar-text">
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Buscar.." aria-label="Search">
+            <button class="btn btn-outline-secondary" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+          </form>
+    </span>
+  </div>
+  </div>
+  `
+  menu.appendChild(nav)
+  
+}
+actualizarCantidadDeProductos(CantidadDeProductos)
 class Producto {
     constructor(nombre,precio,stock,imagen) {
       this.nombre = nombre;
@@ -15,6 +63,8 @@ fetch("./datos.json")
     }) 
 
 let limpiar = 0
+
+
 const galeria = document.getElementById("galeria")
 
 const mostrarProductos = (a) =>{
@@ -47,6 +97,10 @@ const mostrarProductos = (a) =>{
     const indiceEncontrado = carrito.findIndex((elemento)=>{
     return elemento.id === productos[i].id
     })
+
+    CantidadDeProductos = CantidadDeProductos + 1
+    console.log(CantidadDeProductos)
+   
     limpiar =1
     if (indiceEncontrado === -1){
       const nuevoProducto = productos[i]
@@ -58,7 +112,8 @@ const mostrarProductos = (a) =>{
       }
       else{
         carrito.push(nuevoProducto)
-        dibujarcarrito()
+        
+        actualizarCantidadDeProductos(CantidadDeProductos)
         mostrarProductos(productos)
       }          
     }
@@ -71,46 +126,63 @@ const mostrarProductos = (a) =>{
         swal(`No hay stock ${carrito[indiceEncontrado].nombre} `,"", "error");
       }
       else
-      {
-        dibujarcarrito()
+      { 
+        actualizarCantidadDeProductos(CantidadDeProductos)
         mostrarProductos(productos)
       }        
     }
   }
   let total = 0
   const dibujarcarrito = () =>{
-    modalcarrito.innerHTML=" "
+    modalcarrito.innerHTML= `<p class="titulo"><strong>CARRITO DE COMPRAS</strong></p> `
       if(carrito.length >0){
           carrito.forEach((producto,i) =>{
             total = total + (producto.precio*producto.cantidad)
+            
             const carritoContainer = document.createElement("div")
             carritoContainer.className = "carrito";
             carritoContainer.innerHTML =
             `<img src="${producto.imagen}"/>
-            <div>
+            <div class="tituloCarrito">
               ${producto.nombre}
             </div>
-            <div>
-            Cantidad:${producto.cantidad}
+            <div class="tituloCarrito">
+            Cantidad:
             </div>
             <div>
-              Precio:$${producto.precio}
+            ${producto.cantidad}
+            </div>
+            <div class="tituloCarrito">
+              Precio:
             </div>
             <div>
-              Subtotal: $${producto.precio * producto.cantidad}
+              $${producto.precio}
+            </div>
+            <div class="tituloCarrito">
+              Subtotal: 
+            </div>
+            <div class="tituloCarrito">
+              $${producto.precio * producto.cantidad}
             </div>
             <a onclick="eliminarDeCarrito(${i})" class="btn btn-secondary">Eliminar</a>`
             modalcarrito.appendChild(carritoContainer)
         })
         const totalizador = document.createElement("div");
-        totalizador.innerHTML = `<div> total $ ${total}</div>
-        <button class= "btn btn-info" onClick="finalizar()"> Comprar </button>`;
+        totalizador.innerHTML = `<div class="Total"> TOTAL: $ ${total}</div>
+        <button class= "btn btn-warning botonComprar" onClick="finalizar()"> Comprar </button>`;
         modalcarrito.appendChild(totalizador);
+      }
+      else{const carritoContainer = document.createElement("div")
+      carritoContainer.className = "carrito";
+      carritoContainer.innerHTML =`<p>No hay productos</p>`
+      modalcarrito.appendChild(carritoContainer)
       }
   }
   const eliminarDeCarrito = (i) =>{
    carrito.splice(i,1)
    swal("Producto eliminado","", "success");
+   CantidadDeProductos=CantidadDeProductos-1
+   actualizarCantidadDeProductos(CantidadDeProductos)
    dibujarcarrito()
   }
   class datosCompra{
@@ -123,7 +195,7 @@ const mostrarProductos = (a) =>{
   function mostrarMensaje(){
     const datos = localStorage.getItem("datosCompra") 
     const obj = JSON.parse(datos)
-    swal("Compra realizada!", `Gracias ${obj.nombre} Los datos de la compra se enviarán a tu correo ${obj.email}`, "success");
+    swal("Compra realizada! ", `Gracias ${obj.nombre}. Pagaste $${total}. Los datos de la compra se enviarán a tu correo ${obj.email}`, "success");
       
   }
   
@@ -139,26 +211,39 @@ const mostrarProductos = (a) =>{
   }
   const pagar = document.createElement("div");
   const finalizar = (i) =>{
-    pagar.innerHTML = `<div class= "row"> 
+    modalcarrito.innerHTML=""
+    pagar.classList.add("row")
+    pagar.innerHTML = `
+    <p class="titulo"><strong>Ingrese datos para envío</strong></p> 
     <div class="col-lg-6 col-md-12 col-sm-12">
-    <div class="mb-3">
+    <div class="mb-2">
     <label for="exampleInputEmail1" class="form-label">Nombre completo</label>
     <input type="" class="form-control" id="Nombre" aria-describedby="emailHelp">
     </div>
-    <div class="mb-3">
+    <div class="mb-2">
     <label for="" class="form-label">Email</label>
     <input type="email" class="form-control" id="email">
     <div id="emailHelp" class="form-text"></div>
     </div>
-    <div class="mb-3">
+    <div class="mb-2">
     <label for="exampleInputEmail1" class="form-label">Dirección</label>
     <input type="email" class="form-control" aria-describedby="emailHelp" id="direccion">
     </div>
-    <button type="" class="btn btn-warning" id="boton" onclick="cargarDatosCliente()">Enviar</button>
+    <div class="linea">
     </div>
-    <div class="col-lg-6 col-md-12 col-sm-12">
+    <div class="mb-2">
+    <label for="exampleInputEmail1" class="form-label"><strong>Vas a pagar $${total}</strong></label>
+    </div>
+    <div class="mb-2">
+    <label for="exampleInputEmail1" class="form-label">Datos de la tarjeta</label>
+    <input type="tarjeta" class="form-control" aria-describedby="" id="tarjeta">
+    </div>
+    <button type="" class="btn btn-success" id="boton" onclick="cargarDatosCliente()">Enviar</button>
+    </div>
+    <div class="col-lg-6 col-md-12 col-sm-12 ">
     <img src="./imagenes/sweater_1.png"  class="img-fluid"  alt="imagen muffin publicidad"..">
     </div>
     </div>`
     modalcarrito.appendChild(pagar);
   }
+  
